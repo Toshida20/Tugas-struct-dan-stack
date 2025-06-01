0ad1523f3e0393f2047d63d4e1808aaa4886d26b
Toshida20 <Ilhamm1979@gmail.com>
2025-06-01 23:37:06 +0800

    3 - Menambahkan Input pada program

--- C030324062_STRUCT.cpp
+++ C030324062_STRUCT.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <conio.h>
+#include <string>
 using namespace std;
+
 //C030324062_STRUCT
+
 struct Mahasiswa {
     string nim;
     string nama;
@@ -9,18 +12,26 @@ struct Mahasiswa {
     float ipk;
 };
 
+void input(string nim, string nama, string alamat, float ipk) {
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin.ignore(); getline(cin, nama);
+    cout << "Masukkan Alamat: ";
+    getline(cin, alamat);
+    cout << "Masukkan IPK: ";
+    cin >> ipk;
+    cout << "\nTekan enter untuk melanjutkan..."; getch();
+}
+
+
 int main() {
     Mahasiswa jani;
+    string nim = jani.nim;
+    string nama = jani.nama;
+    string alamat = jani.alamat;
+    float ipk = jani.ipk;
 
-    jani.nim = "c030324062";
-    jani.nama = "Ahmad Tijani";
-    jani.alamat = "Pal 8 Manarap";
-    jani.ipk = 3.9;
-
-    cout << jani.nim << "\n";
-    cout << jani.nama << "\n";
-    cout << jani.alamat << "\n";
-    cout << jani.ipk << "\n";
-
+    input(nim,nama,alamat,ipk);
     return 0;
 }
