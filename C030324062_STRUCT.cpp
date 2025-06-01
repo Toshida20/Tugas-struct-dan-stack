a6ab101b296066192286a1431a238942c2e0005c
Toshida20 <Ilhamm1979@gmail.com>
2025-06-02 00:23:07 +0800

    Menambahkan sistem output dan juga menambahkan referensi '&' pada variabel fungsi input agar data yang diinput bisa masuk ke output

--- C030324062_STRUCT.cpp
+++ C030324062_STRUCT.cpp
@@ -2,9 +2,7 @@
 #include <conio.h>
 #include <string>
 using namespace std;
-
 //C030324062_STRUCT
-
 struct Mahasiswa {
     string nim;
     string nama;
@@ -12,11 +10,12 @@ struct Mahasiswa {
     float ipk;
 };
 
-void input(string nim, string nama, string alamat, float ipk) {
+void input(string &nim, string &nama, string &alamat, float &ipk) {
     cout << "Masukkan NIM: ";
     cin >> nim;
+    cin.ignore();
     cout << "Masukkan Nama: ";
-    cin.ignore(); getline(cin, nama);
+    getline(cin, nama);
     cout << "Masukkan Alamat: ";
     getline(cin, alamat);
     cout << "Masukkan IPK: ";
@@ -24,14 +23,29 @@ void input(string nim, string nama, string alamat, float ipk) {
     cout << "\nTekan enter untuk melanjutkan..."; getch();
 }
 
+void output(Mahasiswa data) {
+    cout << "\n=== DATA MAHASISWA ===\n";
+    cout << "NIM    : " << data.nim << endl;
+    cout << "Nama   : " << data.nama << endl;
+    cout << "Alamat : " << data.alamat << endl;
+    cout << "IPK    : " << data.ipk << endl;
+}
 
 int main() {
     Mahasiswa jani;
+
     string nim = jani.nim;
     string nama = jani.nama;
     string alamat = jani.alamat;
     float ipk = jani.ipk;
 
-    input(nim,nama,alamat,ipk);
+    input(nim, nama, alamat, ipk);
+
+    jani.nim = nim;
+    jani.nama = nama;
+    jani.alamat = alamat;
+    jani.ipk = ipk;
+
+    output(jani);
     return 0;
 }
