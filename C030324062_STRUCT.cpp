8e74207dcc2de957e0f82c437039b612d8768e83
Toshida20 <Ilhamm1979@gmail.com>
2025-06-01 22:18:16 +0800

    2 | merubah nama file

---
+++ C030324062_STRUCT.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+//C030324062_STRUCT
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
+int main() {
+    Mahasiswa jani;
+
+    jani.nim = "c030324062";
+    jani.nama = "Ahmad Tijani";
+    jani.alamat = "Pal 8 Manarap";
+    jani.ipk = 3.9;
+
+    cout << jani.nim << "\n";
+    cout << jani.nama << "\n";
+    cout << jani.alamat << "\n";
+    cout << jani.ipk << "\n";
+
+    return 0;
+}
