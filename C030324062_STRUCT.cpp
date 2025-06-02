67495ceeb1080517352f53ee4d25dff33f1c593b
Toshida20 <Ilhamm1979@gmail.com>
2025-06-02 16:29:40 +0800

    Penambahan sistem pointer dan perbaikan pada bagian fungsi input output yang terkadang tidak menampilkan output

--- C030324062_STRUCT.cpp
+++ C030324062_STRUCT.cpp
@@ -3,11 +3,7 @@
 #include <string>
 using namespace std;
 
-//C030324062_STRUCT
-//git branch -M main
-//git push -u origin main
-//git add C030324062_STRUCT.cpp
-//git commit -m "BLA BLA BLA"
+// C030324062_STRUCT
 
 struct Mahasiswa {
     string nim;
@@ -16,51 +12,45 @@ struct Mahasiswa {
     float ipk;
 };
 
-void bersih(){
+void bersih() {
     system("cls");
-};
-
-void input(string &nim, string &nama, string &alamat, float &ipk) {
-    bersih();
-    cout << "=====MASUKAN DATA MAHASISWA=====\n" << endl;
-    cout << "Masukkan NIM: ";
-    cin >> nim;
-    cin.ignore();
-    cout << "Masukkan Nama: ";
-    getline(cin, nama);
-    cout << "Masukkan Alamat: ";
-    getline(cin, alamat);
-    cout << "Masukkan IPK: ";
-    cin >> ipk;
-    cout << "\nTekan enter untuk melanjutkan...";
-    getch();
-
 }
 
-void output(Mahasiswa data) {
+void input(Mahasiswa *kelas) {
     bersih();
-    cout << "\n===== DATA MAHASISWA =====\n";
-    cout << "NIM: " << data.nim << endl;
-    cout << "Nama: " << data.nama << endl;
-    cout << "Alamat: " << data.alamat << endl;
-    cout << "IPK: " << data.ipk << endl;
+    cout << "===== MASUKAN DATA MAHASISWA =====\n" << endl;
+
+    cout << "Masukkan NIM       : ";
+    cin >> kelas->nim;
+    cin.ignore();
+
+    cout << "Masukkan Nama      : ";
+    getline(cin, kelas->nama);
+
+    cout << "Masukkan Alamat    : ";
+    getline(cin, kelas->alamat);
+
+    cout << "Masukkan IPK       : ";
+    cin >> kelas->ipk;
+
+    cout << "\nTekan enter untuk melanjutkan...";
+    getch();
+}
+
+void output(Mahasiswa *data) {
+    bersih();
+    cout << "\n===== DATA MAHASISWA =====" << endl;
+    cout << "NIM    : " << data->nim << endl;
+    cout << "Nama   : " << data->nama << endl;
+    cout << "Alamat : " << data->alamat << endl;
+    cout << "IPK    : " << data->ipk << endl;
 }
 
 int main() {
     Mahasiswa jani;
 
-    string nim = jani.nim;
-    string nama = jani.nama;
-    string alamat = jani.alamat;
-    float ipk = jani.ipk;
+    input(&jani);
+    output(&jani);
 
-    input(nim, nama, alamat, ipk);
-
-    jani.nim = nim;
-    jani.nama = nama;
-    jani.alamat = alamat;
-    jani.ipk = ipk;
-
-    output(jani);
     return 0;
 }
