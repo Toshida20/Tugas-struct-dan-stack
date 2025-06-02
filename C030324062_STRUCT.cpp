9d6622061a7077e3f5a0272440c8c647d3e28f00
Toshida20 <Ilhamm1979@gmail.com>
2025-06-02 16:53:59 +0800

    Penambahan sistem pointer dan perbaikan pada fungsi yang terkadang tidak menampilkan output

--- C030324062_STRUCT.cpp
+++ C030324062_STRUCT.cpp
@@ -2,7 +2,13 @@
 #include <conio.h>
 #include <string>
 using namespace std;
+
 //C030324062_STRUCT
+//git branch -M main
+//git push -u origin main
+//git add C030324062_STRUCT.cpp
+//git commit -m "BLA BLA BLA"
+
 struct Mahasiswa {
     string nim;
     string nama;
@@ -10,42 +16,45 @@ struct Mahasiswa {
     float ipk;
 };
 
-void input(string &nim, string &nama, string &alamat, float &ipk) {
-    cout << "Masukkan NIM: ";
-    cin >> nim;
-    cin.ignore();
-    cout << "Masukkan Nama: ";
-    getline(cin, nama);
-    cout << "Masukkan Alamat: ";
-    getline(cin, alamat);
-    cout << "Masukkan IPK: ";
-    cin >> ipk;
-    cout << "\nTekan enter untuk melanjutkan..."; getch();
+void bersih() {
+    system("cls");
 }
 
-void output(Mahasiswa data) {
-    cout << "\n=== DATA MAHASISWA ===\n";
-    cout << "NIM    : " << data.nim << endl;
-    cout << "Nama   : " << data.nama << endl;
-    cout << "Alamat : " << data.alamat << endl;
-    cout << "IPK    : " << data.ipk << endl;
+void input(Mahasiswa *kelas) {
+    bersih();
+    cout << "===== MASUKAN DATA MAHASISWA =====\n" << endl;
+
+    cout << "Masukkan NIM       : ";
+    cin >> kelas->nim;
+    cin.ignore();
+
+    cout << "Masukkan Nama      : ";
+    getline(cin, kelas->nama);
+
+    cout << "Masukkan Alamat    : ";
+    getline(cin, kelas->alamat);
+
+    cout << "Masukkan IPK       : ";
+    cin >> kelas->ipk;
+
+    cout << "\nTekan enter untuk melanjutkan...";
+    getch();
+}
+
+void output(Mahasiswa *data) {
+    bersih();
+    cout << "\n===== DATA MAHASISWA =====" << endl;
+    cout << "NIM    : " << data->nim << endl;
+    cout << "Nama   : " << data->nama << endl;
+    cout << "Alamat : " << data->alamat << endl;
+    cout << "IPK    : " << data->ipk << endl;
 }
 
 int main() {
     Mahasiswa jani;
 
-    string nim = jani.nim;
-    string nama = jani.nama;
-    string alamat = jani.alamat;
-    float ipk = jani.ipk;
+    input(&jani);
+    output(&jani);
 
-    input(nim, nama, alamat, ipk);
-
-    jani.nim = nim;
-    jani.nama = nama;
-    jani.alamat = alamat;
-    jani.ipk = ipk;
-
-    output(jani);
     return 0;
 }
